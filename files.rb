b37fa0a35b79ef8abdaa57cf8f0837d9243a47ad
Nico Saueressig <nico@nicosaueressig.de>
2014-04-09 11:56:16 +0100

    done

--- files.rb
+++ files.rb
@@ -1,16 +1,20 @@
 require 'color_text'
 
-puts "Hey, there, go ahead, write  something and we´ll put it into a file for you!".neon
+puts "Hey, there, go ahead, write something and then decide what to do with it. If you don´t want to enter any input, just press return".purple
 input = gets.chomp
-#create new file hello_from_ruby
+
 puts "Type:
 'save' - to save input to file
+'add' - add your input to the file
 'read' - read the current file
-'introspect' - print program out on screen"
+'introspect' - print program out on screen".neon
 command = gets.chomp
+
+content_read = lambda {|content| content.read}
 
 case command
 	when "save" then File.open("hello_from_ruby.txt", "w"){ |content| content.write input}
+	when "add" then File.open("hello_from_ruby.txt", "a"){ |content| content.write input}
 	when "read" then puts File.open("hello_from_ruby.txt"){|content| content.read}
-	when "introspect" then puts File.open("files.rb"){|content| content.read}
+	when "introspect" then puts File.open("files.rb"){|content| content.read}.green
 end
