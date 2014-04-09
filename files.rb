1a3f068e4b335a18b9c61edf4181efb55837abb2
Nico Saueressig <nico@nicosaueressig.de>
2014-04-09 11:30:49 +0100

    added 2 and 3

--- files.rb
+++ files.rb
@@ -3,4 +3,12 @@ require 'color_text'
 puts "Hey, there, go ahead, write  something and we´ll put it into a file for you!".neon
 input = gets.chomp
 #create new file hello_from_ruby
-File.open("hello_from_ruby.txt", "w"){ |content| content.write input}
+puts "Type:
+'save' - to save input to file
+'read' - read the current file"
+command = gets.chomp
+
+case command
+	when "save" then File.open("hello_from_ruby.txt", "w"){ |content| content.write input}
+	when "read" then puts File.open("hello_from_ruby.txt"){|content| content.read}
+end
