9a2417b59db01bfe456499b5a5dc5663831f6d0e
Nico Saueressig <nico@nicosaueressig.de>
2014-04-09 11:25:31 +0100

    challenge 2

--- files.rb
+++ files.rb
@@ -1,2 +1,6 @@
-file = File.open("hello_from_ruby")
-#puts file.read
+require 'color_text'
+
+puts "Hey, there, go ahead, write  something and we´ll put it into a file for you!".neon
+input = gets.chomp
+#create new file hello_from_ruby
+File.open("hello_from_ruby.txt", "w"){ |content| content.write input}
